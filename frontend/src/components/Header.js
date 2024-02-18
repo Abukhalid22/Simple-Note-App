56221a821490ac974d90cfa9ef28616bbb6df79e
Abukhalid <nasir.hussein94@hotmail.com>
2024-02-18 00:38:13 +0000

    Argocd Integration attempt 3

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="app-header">
-        <h1>My Notes (New Header)</h1>
+        <h1>My Notes (New Header 3)</h1>
     </div>
   )
 }
