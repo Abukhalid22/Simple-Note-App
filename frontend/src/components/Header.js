62f6857c9b4529a5c96482f748b7ca58af890431
Abukhalid <nasir.hussein94@hotmail.com>
2024-02-18 03:46:07 +0000

    argocd integration and enabled workflow read access

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="app-header">
-        <h1>My Notes (New Header 8)</h1>
+        <h1>My Notes (New Header 10)</h1>
     </div>
   )
 }
