991bdba9d95aa51b61f383dd2cc96c4cdffa153e
Abukhalid <nasir.hussein94@hotmail.com>
2024-03-17 04:11:33 +0000

    Working on AWS

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="app-header">
-        <h1>My Notes (New Header 20:50)</h1>
+        <h1>My Notes (New Header 04:11)</h1>
     </div>
   )
 }
