b2cb07ec5ef6b142fb834515c64330173f215b73
Abukhalid <nasir.hussein94@hotmail.com>
2024-03-26 00:31:56 +0000

    Snyk Integration and Settings.py secret enhancement

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r-z-hjqp24h+2n-u8uv6gg)ht2d0jla7^8p068f9)5xp8k_rep'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
